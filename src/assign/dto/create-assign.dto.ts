540e3d0115f546ebfa7a33b5836d77c3ef60f711
Tran Khai Hoang <hoangbo74@gmail.com>
2023-11-26 20:03:52 +0700

    validation create assign dto

--- src/assign/dto/create-assign.dto.ts
+++ src/assign/dto/create-assign.dto.ts
@@ -1,7 +1,12 @@
+import { IsNotEmptyObject } from "class-validator";
 import { Types } from "mongoose";
 
 export class CreateAssignDto {
+  @IsNotEmptyObject()
   readonly leader: { type: Types.ObjectId; ref: "UserSchema" };
+
+  @IsNotEmptyObject()
   readonly task: { type: Types.ObjectId; ref: "TaskSchema" };
+
   readonly attachments: [string];
 }
--- src/assign/schema/assign.schema.ts
+++ src/assign/schema/assign.schema.ts
@@ -1,15 +1,15 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Types } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Types } from "mongoose";
 
 @Schema({
   timestamps: true,
 })
 export class Assign {
-  @Prop({ required: true, type: Types.ObjectId, ref: 'UserSchema' })
-  leader: { type: Types.ObjectId; ref: 'UserSchema' };
+  @Prop({ required: true, type: Types.ObjectId, ref: "UserSchema" })
+  leader: { type: Types.ObjectId; ref: "UserSchema" };
 
-  @Prop({ type: Types.ObjectId, ref: 'TaskSchema' })
-  task: { type: Types.ObjectId; ref: 'TaskSchema' };
+  @Prop({ type: Types.ObjectId, ref: "TaskSchema" })
+  task: { type: Types.ObjectId; ref: "TaskSchema" };
 
   @Prop()
   attachments: [string];
