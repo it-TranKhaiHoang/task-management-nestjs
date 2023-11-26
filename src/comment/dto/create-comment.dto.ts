52a8577b9b228cbd864f640a0558c4d50e2d269e
Tran Khai Hoang <hoangbo74@gmail.com>
2023-11-26 20:23:39 +0700

    create comment dto

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCommentDto {}
+import { IsNotEmptyObject } from "class-validator";
+import { Types } from "mongoose";
+
+export class CreateCommentDto {
+  @IsNotEmptyObject()
+  project: { type: Types.ObjectId; ref: "Project" };
+
+  @IsNotEmptyObject()
+  author: { type: Types.ObjectId; ref: "User" };
+
+  body: string;
+}
