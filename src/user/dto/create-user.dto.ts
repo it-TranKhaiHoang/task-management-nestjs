a5641f2c61b1a3bfad3f1ee1beddd0711f6769bc
Tran Khai Hoang <hoangbo74@gmail.com>
2023-11-26 19:58:41 +0700

    add class-validator to create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,21 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
 import { Role } from "../schemas/user.schema";
-
 export class CreteUserDto {
+  @IsNotEmpty()
   readonly fullname: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
+
+  @IsNotEmpty()
   readonly status: string;
+
+  @IsNotEmpty()
   readonly position: string;
+
+  @IsNotEmpty()
   readonly role: Role;
 }
