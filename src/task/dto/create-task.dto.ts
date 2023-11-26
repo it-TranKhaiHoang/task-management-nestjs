8f26c726a58127bb06cfa82e5a02318e7446345f
Tran Khai Hoang <hoangbo74@gmail.com>
2023-11-26 20:19:36 +0700

    create task dto

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTaskDto {}
+import { Types } from "mongoose";
+import { IsNotEmpty, IsNotEmptyObject } from "class-validator";
+
+export class CreateTaskDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmptyObject()
+  project: { type: Types.ObjectId; ref: "Project" };
+
+  description: string;
+
+  @IsNotEmptyObject()
+  members: [{ type: Types.ObjectId; ref: "User" }];
+
+  @IsNotEmpty()
+  start_date: Date;
+
+  @IsNotEmpty()
+  end_date: Date;
+
+  attachments: [string];
+}
