3b3c561906cf717299d5d4585bc37436c29232cf
Tran Khai Hoang <hoangbo74@gmail.com>
2023-11-26 20:08:23 +0700

    validation login and register dto

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,10 +1,21 @@
 import { Role } from "src/user/schemas/user.schema";
-
+import { IsEmail, IsNotEmpty } from "class-validator";
 export class RegisterUserDto {
+  @IsNotEmpty()
   readonly fullname: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
+
+  @IsNotEmpty()
   readonly status: string;
+
+  @IsNotEmpty()
   readonly position: string;
+
+  @IsNotEmpty()
   readonly role: Role;
 }
